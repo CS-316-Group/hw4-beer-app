98337b5e6d64a4a0a50b19427268026cee985278
Caroline <carolinewang01@gmail.com>
2020-03-01 13:46:00 -0500

    replaced all files with teh sample flask app files

--- README.md
+++
@@ -1,7 +0,0 @@
-First step. Create a virtual environment through anaconda. conda create -n flask_app python=3.7.3
-
-Install all the dependencies in your application pip install -r requirements.txt (when is available) dependencies is are all the pip libraries needed eg. flask, sqlalchemy. 2a. To generate requirements.text run pip freeze >> requirements.txt run this anytime you run a new library.
-
-Export Flask app.
-
-export FLASK_APP=flask_app.py and then flask run# hw4-beer-app
--- app.py
+++ app.py
@@ -1,22 +1,46 @@
-from flask import Flask, request, redirect, url_for
-from flask import render_template
-from database import db
+from flask import Flask, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import models
+import forms
 
 app = Flask(__name__)
+app.secret_key = 's3cr3t'
+app.config.from_object('config')
+db = SQLAlchemy(app, session_options={'autocommit': False})
 
+@app.route('/')
+def all_drinkers():
+    drinkers = db.session.query(models.Drinker).all()
+    return render_template('all-drinkers.html', drinkers=drinkers)
 
-@app.route('/serves', methods=['GET', 'POST'])
-def serves():
-	beer_names = @TODO
-	form = forms.ServingsFormFactory.form( @TODO )
-	if form.@TODO():
-		return @TODO('/servings/' + form.beer_sel.data)
-	return render_template('serves.html', form=form)
+@app.route('/drinker/<name>')
+def drinker(name):
+    drinker = db.session.query(models.Drinker)\
+        .filter(models.Drinker.name == name).one()
+    return render_template('drinker.html', drinker=drinker)
 
-@app.route('/servings/<beer_name>')
-def servings_for(beer_name):
-	results = db.session.query(models.Serves, models.Bar) \
-				.filter(@TODO) \
-				.join(@TODO).all()
-	return render_template('servings_for.html', beer_name=beer_name,
-							data=results)
+@app.route('/edit-drinker/<name>', methods=['GET', 'POST'])
+def edit_drinker(name):
+    drinker = db.session.query(models.Drinker)\
+        .filter(models.Drinker.name == name).one()
+    beers = db.session.query(models.Beer).all()
+    bars = db.session.query(models.Bar).all()
+    form = forms.DrinkerEditFormFactory.form(drinker, beers, bars)
+    if form.validate_on_submit():
+        try:
+            form.errors.pop('database', None)
+            models.Drinker.edit(name, form.name.data, form.address.data,
+                                form.get_beers_liked(), form.get_bars_frequented())
+            return redirect(url_for('drinker', name=form.name.data))
+        except BaseException as e:
+            form.errors['database'] = str(e)
+            return render_template('edit-drinker.html', drinker=drinker, form=form)
+    else:
+        return render_template('edit-drinker.html', drinker=drinker, form=form)
+
+@app.template_filter('pluralize')
+def pluralize(number, singular='', plural='s'):
+    return singular if number in (0, 1) else plural
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
