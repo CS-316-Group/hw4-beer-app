79ec0d2482437b1498f5c9ea93455b1805b1c071
Elise Brown <eliseg.brown@gmail.com>
2020-03-03 23:41:34 -0500

    updated websiiiiiite

--- app.py
+++ app.py
@@ -45,8 +45,8 @@ def serves():
     dropdown_list = []
     for beer in beer_names:
         dropdown_list.append(beer[0])
-    # if form.validate_on_submit():
-    #     return redirect(url_for('/servings/' + form.beer_sel.data)) # not sure if this is right
+    if form.validate_on_submit():
+        return redirect('/servings/' + form.beer_sel.data) # not sure if this is right
     return render_template('serves.html', dropdown_list=dropdown_list, form=form)
 
 @app.route('/servings/<beer_name>')
@@ -54,14 +54,25 @@ def servings_for(beer_name):
     # filter to get data for the beer we are concerned with 
     # join the two tables 
     # display only the bar name, bar address, and beer price
-    results = db.session.query(models.Serves, models.Bar) \
-                        .filter(models.Serves.beer == beer_name,  
-                                models.Bar.serves == beer_name) \
-                        .join(models.Serves.bar == models.Bar.name)\
-                        .options(load_only(models.Bar.name, 
-                                           models.Bar.address, 
-                                           models.Serves.price)) \
-                        .all() 
+    # results = db.session.query(models.Serves).join(models.Bar)
+    # results = results.filter(models.Serves.beer == beer_name).all
+    #                     # .filter(models.Serves.beer == beer_name).all()
+    #                     # .join(models.Serves.bar == models.Bar.name).all()
+    #                     # .options(load_only(models.Bar.name, 
+    #                     #                    models.Bar.address, 
+    #                     #                    models.Serves.price)) \
+    #                     # .all() 
+
+    # results = db.session.query(models.Serves, models.Bar).all()
+    results=db.session.query(models.Serves).join(models.Bar, models.Bar.name== models.Serves.bar).filter(models.Serves.beer == beer_name).all()
+    # \
+    #                     .filter(models.Serves.beer == beer_name,
+    #                             models.Bar.serves == beer_name).all()
+    #                     .join(models.Serves.bar == models.Bar.name)\
+    #                     .options(load_only(models.Bar.name,
+    #                                        models.Bar.address,
+    #                                        models.Serves.price)) \
+    #                     .all()
     return render_template('servings_for.html', 
                             beer_name=beer_name,
                             data=results)
